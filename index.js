97acd773d684dea7f88cc766352aa62ad5147d78
Daniel Nadel <dnadel95@gmail.com>
2021-09-29 15:35:31 +0300

    removed superfluous files

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const 	path = require('path');
 const 	busboy = require('connect-busboy'); //Parse form data
 const 	port = process.env.PORT || 8080;
 const 	host = process.env.BASE_URL || 'http://127.0.0.1';
-const	mysql = require('mysql');
-//const	conn = require('./db/conn');
 
 const app = express();
 
@@ -21,15 +19,14 @@ app.use(bodyParser.json({limit: '10000000mb'}));
 app.use(bodyParser.urlencoded({limit: '10000000mb', extended: true}));
 app.set('appName', 'canditech');
 
-//let c = conn.init();
-//conn.connect(c);
-
 const db = require("./models");
-//db.sequelize.sync();
+db.sequelize.sync();
 //Reload tables
+/*
 db.sequelize.sync({ force: true }).then(() => {
 	console.log("Drop and re-sync db.");
 });
+*/
 app.set('views', path.join(__dirname + '/HTML'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
